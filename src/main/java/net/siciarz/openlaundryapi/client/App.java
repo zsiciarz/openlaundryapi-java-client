256d248a7840a3fb099a09582b018613d0812d68
Zbigniew Siciarz <antyqjon@gmail.com>
2013-07-24 22:43:16 +0200

    Going out and fetching real data from the API.

--- src/main/java/net/siciarz/openlaundryapi/client/App.java
+++ src/main/java/net/siciarz/openlaundryapi/client/App.java
@@ -1,5 +1,9 @@
 package net.siciarz.openlaundryapi.client;
 
+import java.net.URL;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  * Entry point to the application.
@@ -8,7 +12,14 @@ package net.siciarz.openlaundryapi.client;
 public class App {
     public static void main(String[] args) {
         System.out.println("Open Laundry API Java client");
-        Device device = new Device("666", "My device");
-        System.out.println(device.getName());
+        URL apiUrl;
+        try {
+            apiUrl = new URL("http://openlaundryapi.org/api/device/666/");
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> map = mapper.readValue(apiUrl, Map.class);
+            System.out.println(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
